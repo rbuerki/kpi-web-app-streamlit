76637024503b636d3a48261a7e2f18ff86193337
rbuerki <r2d4@bluewin.ch>
2020-06-16 21:17:50 +0200

    (Re)Move files

--- tests/context.py
+++
@@ -1,9 +0,0 @@
-# Helps with imports of source code files by manipulating class path, see here for more:
-# https://towardsdatascience.com/ultimate-setup-for-your-next-python-project-179bda8a7c2c
-
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-import kpi_sheet  # noqa: E402, F401
