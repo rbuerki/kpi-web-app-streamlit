e935e67ebbd2261f2609acc85928e3db43a820ff
rbuerki <r2d4@bluewin.ch>
2020-06-28 14:07:02 +0200

    Fix sys path (again)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import pytest
 
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+
 
 @pytest.fixture
 def data():
